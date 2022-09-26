6bb506d3d5ff7549526780d76c1265c6f7a83945
Antti <anttikatainen4@gmail.comL>
2022-09-26 12:21:14 +0300

    topics tekstit toimii

--- ryhmatyo/feedback/models.py
+++ ryhmatyo/feedback/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Topic(models.Model):
     name = models.CharField(max_length=160)
-   
-    def _str_(self):
-        return self.name
+    def __str__(self):
+        return str(self.name)
     
 
 
@@ -19,7 +18,7 @@ class Feedback(models.Model):
         (4, '4'),
         (5, '5'),
         )
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
     rating = models.IntegerField(default=1, choices=RATING_CHOICES)
     good = models.TextField(max_length=2000,blank=True )
     bad = models.TextField(max_length=2000,blank=True )
