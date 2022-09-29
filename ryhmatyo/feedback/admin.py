667042e9967756c2ebb13c134a450c1df31cf712
Antti <anttikatainen4@gmail.comL>
2022-09-30 01:59:59 +0300

    avg melkein toimii

--- ryhmatyo/feedback/admin.py
+++ ryhmatyo/feedback/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import Topic, Feedback
 from django.contrib.auth.models import User
+from django.db.models import Avg
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    
+    list_display = ("avg",'name')
     search_fields=['name']
 class FeedbackAdmin(admin.ModelAdmin):
+    
     list_display = ("author",'topic','rating','good','bad','ideas','date')
     list_filter=['topic','date']
     search_fields=['good','bad']
--- ryhmatyo/feedback/models.py
+++ ryhmatyo/feedback/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
-class Topic(models.Model):
-    name = models.CharField(max_length=160)
-    def __str__(self):
-        return str(self.name)
+from django.db.models import Avg
+
+
+
     
 
 
 
 
 class Feedback(models.Model):
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
     RATING_CHOICES = (
         
         (1, '1'),
@@ -27,8 +27,19 @@ class Feedback(models.Model):
     bad = models.TextField(max_length=2000,blank=True )
     ideas = models.TextField(max_length=2000,blank=True )
     date =models.DateTimeField(auto_now_add=True)
-    
+   
+    class meta:
+        unique_together = [['author', 'topic']]
     def _str_(self):
         return f"{self.date}"
+
+class Topic(models.Model):
+    avg =str(Feedback.objects.aggregate(Avg('rating')))
+    name = models.CharField(max_length=160)
+    def __str__(self):
+        return str(self.name)
+
+
+  
 
 # Create your models here.
